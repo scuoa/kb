d6657d7b0f8ff60705d7310ac58bc51365e8faea
scuoa <114360999+scuoa@users.noreply.github.com>
2024-02-18 23:20:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Home</title>
+		<meta name="language" content="en" />  
+		<meta name="description" content="" />  
+		<meta name="keywords" content="" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+  		<link href="css/site.css" rel="stylesheet"> 
+  		<script src="js/jquery.min.js" ></script>
+  		<script src="js/site.js" ></script>
+
+	</head>
+	<body>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">ㅤMenu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="nayeon.php">Nayeon</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="jeongyeon.php">Jeongyeon</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="momo.php">Momo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sana.php">Sana</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="jihyo.php">Jihyo</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="mina.php">Mina</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="dahyun.php">Dahyun</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="chaeyoung.php">Chaeyoung</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="tzuyu.php">Tzuyu</a>
+        </li>
+      </ul>
+    </div>
+  </nav>  
+
+<div class="container mt-3">
+<h3 class="text-center">Home page</h3>
+</div>
+
+
+
+</body>
+</html>
